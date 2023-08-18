cfb5a1643fd2531525736620f7594823927e91e3
Enriyuu <gm3r23@gmail.com>
2023-08-17 22:46:21 -0300

    .

--- aula1.py
+++ aula1.py
@@ -1,1 +1,12 @@
-print("diego é ay")
+nome = input("Qual o seu nome?")
+sobrenome = input("Qual o seu sobrenome?")
+idade = int(input("Qual a sua idade?"))
+ano_de_nascimento = input("Qual o seu ano de nascimento?")
+altura = input("Qual a sua altura?")
+print("O nome do usuário é:", nome, sobrenome)
+print("A idade do usuário é: ,", idade, "Nascida em: ", ano_de_nascimento)
+if idade < 18:
+    print("O usuário é de menor")
+elif idade >= 18:
+    print("O usuário é de maior: ")
+print("A altura do usuário é de: ", altura)
